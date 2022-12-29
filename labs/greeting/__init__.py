c04e420fd2c8a040d0e4beca7b8c87dde934b816
Mohamed Alkobrosli <93363076+kobros-tech@users.noreply.github.com>
2022-12-29 09:17:18 +0200

    Update __init__.py

--- labs/greeting/__init__.py
+++ labs/greeting/__init__.py
@@ -3,4 +3,4 @@ import check50
 @check50.check()
 def hello_world():
   """hello world"""
-  check50.run("python3 hello.py").stdout("Hello, world!", regex=False).exit(0)
+  check50.run("bash greeting.sh").stdout("Hello, Linux!", regex=False).exit(0)
