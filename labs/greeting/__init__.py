4a6402505daf31debd7ec426ed8f3731d14f7e8a
Mohamed Alkobrosli <93363076+kobros-tech@users.noreply.github.com>
2022-12-29 09:24:29 +0200

    Update __init__.py

--- labs/greeting/__init__.py
+++ labs/greeting/__init__.py
@@ -1,6 +1,11 @@
 import check50
 
 @check50.check()
+def exists():
+    """greeting.sh exists"""
+    check50.exists("greeting.sh")
+    
+@check50.check()
 def hello_world():
-  """hello world"""
+  """hello linux"""
   check50.run("bash greeting.sh").stdout("Hello, Linux!", regex=False).exit(0)
