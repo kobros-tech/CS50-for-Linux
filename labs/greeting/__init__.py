bb69b9bd19a6870e7c932e49efaa81efe4440811
Mohamed Alkobrosli <93363076+kobros-tech@users.noreply.github.com>
2022-12-29 19:42:36 +0200

    Update __init__.py

--- labs/greeting/__init__.py
+++ labs/greeting/__init__.py
@@ -2,10 +2,10 @@ import check50
 
 @check50.check()
 def exists():
-    """greeting.c exists"""
-    check50.exists("greeting.c")
-    
-@check50.check()
-def hello_world():
-  """hello linux"""
-  check50.run("bash greeting.c").stdout("Hello, Linux!", regex=False).exit(0)
+    """greeting.sh exists"""
+    check50.exists("greeting.sh")
+
+@check50.check(compiles)
+def greeting():
+    """greeting succeeds"""
+    check50.run("bash greeting.sh").stdout("Hello, Linux!").exit()
