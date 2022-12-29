d605bfcd8f53f28241fe646486389281825c581b
Mohamed Alkobrosli <93363076+kobros-tech@users.noreply.github.com>
2022-12-29 19:59:37 +0200

    Update __init__.py

--- labs/greeting/__init__.py
+++ labs/greeting/__init__.py
@@ -5,7 +5,7 @@ def exists():
     """greeting.sh exists"""
     check50.exists("greeting.sh")
 
-@check50.check(compiles)
+@check50.check()
 def greeting():
     """greeting succeeds"""
     check50.run("bash greeting.sh").stdout("Hello, Linux!").exit()
