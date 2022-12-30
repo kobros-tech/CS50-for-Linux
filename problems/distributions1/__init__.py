95a44a492757c8bc1a796293bec6253d4d42b35c
Mohamed Alkobrosli <93363076+kobros-tech@users.noreply.github.com>
2022-12-30 14:56:08 +0200

    Create __init__.py

---
+++ problems/distributions1/__init__.py
@@ -0,0 +1,11 @@
+import check50
+
+@check50.check()
+def exists():
+    """distributions1.sh exists"""
+    check50.exists("distributions1.sh")
+
+@check50.check()
+def greeting():
+    """distro family names succeeds"""
+    check50.run("bash distributions1.sh").stdout("The Red Hat Family\\nRHEL\\nCentOS\\nFedora\\nOracle Linux\\n\\nThe SUSE Family\\nSLES\\nopenSUSE\\n\\nThe Debian Family\\nDebian\\nUbuntu\\nLinux Mint").exit()
