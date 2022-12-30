8ec3db43cb4a6f545069f82bcf272d4b53f0def0
Mohamed Alkobrosli <93363076+kobros-tech@users.noreply.github.com>
2022-12-30 15:14:17 +0200

    Update __init__.py

--- problems/distributions1/__init__.py
+++ problems/distributions1/__init__.py
@@ -8,4 +8,10 @@ def exists():
 @check50.check()
 def greeting():
     """distro family names succeeds"""
-    check50.run("bash distributions1.sh").stdout("The Red Hat Family\\nRHEL\\nCentOS\\nFedora\\nOracle Linux\\n\\nThe SUSE Family\\nSLES\\nopenSUSE\\n\\nThe Debian Family\\nDebian\\nUbuntu\\nLinux Mint").exit()
+    solution = open("solution.txt", "r")
+    check50.run("bash distributions1.sh").stdout(solution.read()).exit()
+
+@check50.check()
+def greeting():
+    """distro family names succeeds"""
+    check50.run("bash distributions1.sh").stdout("The Red Hat Family\nRHEL\nCentOS\nFedora\nOracle Linux\n\nThe SUSE Family\nSLES\nopenSUSE\n\nThe Debian Family\nDebian\nUbuntu\nLinux Mint").exit()
